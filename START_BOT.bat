beebfa540b3cc37cfada280dd77e66c28d76e69d
thaohochoi-max <thaohochoi@gmail.com>
2026-06-28 06:03:16 +0700

    Remove hardcoded token from START_BOT.bat

--- START_BOT.bat
+++ START_BOT.bat
@@ -1,11 +1,3 @@
 @echo off
-set TELEGRAM_BOT_TOKEN=8985559957:TELEGRAM_TOKEN_REMOVED
-set SHOPEE_AFFILIATE_ID=17332610461
-set SHOPEE_SUB_ID=default
-set ADMIN_SECRET=admin123
-set FB_VERIFY_TOKEN=shopee_webhook_2026
-set FB_PAGE_ID=61590774189281
-set PORT=3000
-
 cd /d "c:\Users\thao\OneDrive\Máy tính\shopee-cashback\backend"
 node server.js
